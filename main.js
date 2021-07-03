17b5b85a00b8038576c9d2f44c7d40f0e363a3df
Sivakumar <shivku.goku@gmail.com>
2021-07-04 00:30:55 +0530

    Writing to firestore.

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hello world!!")
+const admin = require("firebase-admin")
+
+const path = require("path");
+
+admin.initializeApp({
+    credential: admin.credential.cert("./firebase-credentials.json")
+})
+
+const firestore = admin.firestore()
+
+writeToFirestore();
+
+function writeToFirestore() {
+    firestore
+        .collection("test")
+        .doc("new")
+        .set({"id": 1})
+        .then((result, err) => console.log(result))
+
+}
